49b01636ed2ed03540e752d6d2041ac57506715e
Ormelia <ormeliarobinson@yahoo.com>
2020-04-22 19:30:42 -0400

    renamed products.js to server.js to stop confusing myself

--- index.js
+++ index.js
@@ -9,27 +9,29 @@ app.use(express.static(__dirname + 'products'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-const express = require('express');
-const app = express();
 const path = require('path');
-const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/products', function (req, res){
+    //res.sendFile(path.join(__dirname, 'frontend/home.html'))
+    res.send(req.body)
+})
+app.get('products/:id', function (req, res){
     res.sendFile(path.join(__dirname, 'frontend/home.html'))
-});
-
-app.post('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/productInfo.html'))
-});
-
-app.patch('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/productInfo.html'))
-});
-
-app.delete('/', (req, res) => {
+    //res.send(req.params)
+})
+app.post('/products', function (req, res){
     res.sendFile(path.join(__dirname, 'frontend/home.html'))
-});
+    //res.send(req.body)
+})
+app.patch('products/:id', function (req, res){
+    res.sendFile(path.join(__dirname, 'frontend/home.html'))
+    //res.send(req.params, req.body)
+})
+app.delete('products/:id', function (req, res){
 
+    res.send( req.params)
+})
+
+    
 console.log('Server is listening on port ' + port);
 app.listen(port);
